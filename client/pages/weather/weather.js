6638e846e5dc9519cd29296e0c7ead6ab785b735
XiaoCai20180701 <421586311@qq.com>
2019-06-10 15:19:26 +0800

    获取和风天气接口数据
    1.添加 分享的文案  2.设置页面中今明、一天逐时、一周天气数据 3.添加一周天气折线图

--- client/pages/weather/weather.js
+++ client/pages/weather/weather.js
@@ -6,8 +6,11 @@ import {
   getWeather,
   getAir
 } from '../../lib/api-mock'
+import { fixChart, getChartConfig } from '../../lib/utils'
+import Chart from '../../lib/chartjs/chart'
 //获取应用实例
 const app = getApp()
+const CHART_CANVAS_HEIGHT = 272 / 2
 let isUpdate = false
 
 Page({
@@ -24,10 +27,12 @@ Page({
     },
     today: {
       temp: 'N/A',
+      icon: 'none',
       weather: '暂无'
     },
     tomorrow: {
       temp: 'N/A',
+      icon: 'none',
       weather: '暂无'
     },
     // hourlyData
@@ -52,12 +57,18 @@ Page({
       }
       // console.log(e.message, e)
       wx.showToast({
-        title: '加载失败，请稍后再试',
+        title: '获取天气失败，请稍后再试',
         icon: 'none',
         duration: 3000
       })
     }
-    const { lat, lon, province, city, county } = this.data
+    const {
+      lat,
+      lon,
+      province,
+      city,
+      county
+    } = this.data
     getWeather(lat, lon)
       .then((res) => {
         wx.hideLoading()
@@ -82,7 +93,7 @@ Page({
           })
         }
       })
-      .catch((e) => { })
+      .catch((e) => {})
 
     // 获取心情
     // getMood(province, city, county, (res) => {
@@ -119,11 +130,19 @@ Page({
         // console.log(1, res, result)
 
         if (res.statusCode === 200 && result && result.address) {
-          let { address, formatted_addresses, address_component } = result
+          let {
+            address,
+            formatted_addresses,
+            address_component
+          } = result
           if (formatted_addresses && (formatted_addresses.recommend || formatted_addresses.rough)) {
             address = formatted_addresses.recommend || formatted_addresses.rough
           }
-          let { province, city, district: county } = address_component
+          let {
+            province,
+            city,
+            district: county
+          } = address_component
           this.setData({
             province,
             county,
@@ -140,7 +159,11 @@ Page({
     )
   },
   updateLocation(res) {
-    let { latitude: lat, longitude: lon, name } = res
+    let {
+      latitude: lat,
+      longitude: lon,
+      name
+    } = res
     let data = {
       lat,
       lon
@@ -164,8 +187,14 @@ Page({
   chooseLocation() {
     wx.chooseLocation({
       success: (res) => {
-        let { latitude, longitude } = res
-        let { lat, lon } = this.data
+        let {
+          latitude,
+          longitude
+        } = res
+        let {
+          lat,
+          lon
+        } = this.data
         if (latitude == lat && lon == longitude) {
           this.getWeatherData()
         } else {
@@ -183,7 +212,9 @@ Page({
   },
   onLocation() {
     wx.getSetting({
-      success: ({ authSetting }) => {
+      success: ({
+        authSetting
+      }) => {
         can = authSetting['scope.userLocation']
         if (can) {
           this.chooseLocation()
@@ -194,7 +225,10 @@ Page({
     })
   },
   indexDetail(e) {
-    const { name, detail } = e.currentTarget.dataset
+    const {
+      name,
+      detail
+    } = e.currentTarget.dataset
     wx.showModal({
       title: name,
       content: detail,
@@ -220,9 +254,15 @@ Page({
     const currentPage = pages[pages.length - 1] //获取当前页面的对象
     const query = currentPage.options
     if (query && query.address && query.lat && query.lon) {
-      let { province, city, county, address, lat, lon } = query
-      this.setData(
-        {
+      let {
+        province,
+        city,
+        county,
+        address,
+        lat,
+        lon
+      } = query
+      this.setData({
           city,
           province,
           county,
@@ -245,44 +285,98 @@ Page({
       wx.stopPullDownRefresh()
     })
   },
+  onShareAppMessage() {
+    // 如果获取数据失败，则没有位置和天气信息，那么需要个默认文案
+    if (!isUpdate){
+      return {
+        title: '我发现一个好玩的天气小程序，分享给你看看！',
+        path: '/pages/weather/weather'
+      }
+    }else {
+      const {lat, lon, address, province, city, county} = this.data
+      return {
+        title: `「${address}」现在天气情况，快打开看看吧！`,
+        path: '/pages/weather/weather?lat=${lat}&lon=${lon}&address=${address}&province=${province}&city=${city}&county=${county}'
+      }
+    }
+    //如果有天气信息，那么需要给 path 加上天气信息
+  },
   //
   render(data) {
     isUpdate = true
     console.log("render---------", data)
-    const { width, scale } = this.data
-    const { current, lifeStyle, oneWord = '' } = data
-    const { backgroundColor, backgroundImage } = current
-
-    // const _today = daily[0],
-    //   _tomorrow = daily[1]
-    // const today = {
-    //   temp: `${_today.minTemp}/${_today.maxTemp}°`,
-    //   icon: _today.dayIcon,
-    //   weather: _today.day
-    // }
-    // const tomorrow = {
-    //   temp: `${_tomorrow.minTemp}/${_tomorrow.maxTemp}°`,
-    //   icon: _tomorrow.dayIcon,
-    //   weather: _tomorrow.day
-    // }
-
-    // daily.forEach((v) => {
-    //   v.time = v.time + 24 * 60 * 60 * 1000
-    // })
-    // console.log(hourly)
-    this.setData({
-      //  hourlyData: hourly,
-      //   weeklyData: daily,
-      current: current,
-      backgroundImage,
+    const {
+      width,
+      scale
+    } = this.data
+    const {
+      current,
+      daily,
+      hourly,
+      lifeStyle,
+      oneWord = ''
+    } = data
+    const {
       backgroundColor,
-        // today,
-        // tomorrow,
+      backgroundImage
+    } = current
+
+    const _today = daily[0],
+      _tomorrow = daily[1]
+    const today = {
+      temp: `${_today.minTemp}/${_today.maxTemp}°`,
+      icon: _today.dayIcon,
+      weather: _today.day
+    }
+    const tomorrow = {
+      temp: `${_tomorrow.minTemp}/${_tomorrow.maxTemp}°`,
+      icon: _tomorrow.dayIcon,
+      weather: _tomorrow.day
+    }
+    daily.forEach((v) => {
+      v.time = v.time + 24 * 60 * 60 * 1000
+    })
+    this.setData({
+      hourlyData: hourly,
+      weeklyData: daily,
+      current: current,
+      backgroundImage:backgroundImage,
+      backgroundColor:backgroundColor,
+      today:today,
+      tomorrow:tomorrow,
       oneWord: oneWord,
       lifeStyle: lifeStyle
     })
 
+    //chart画图
+    this.drawChart()
   },
+  drawChart() {
+    const { width, scale, weeklyData } = this.data
+    let height = CHART_CANVAS_HEIGHT * scale
+    let ctx = wx.createCanvasContext('chart')
+    fixChart(ctx,width,height)
+    // 添加温度
+    // Chart.pluginService.register({
+    //   afterDatasetsDraw(e, t) {
+    //     ctx.setTextAlign('center')
+    //     ctx.setTextBaseline('middle')
+    //     ctx.setFontSize(16)
 
+    //     e.data.datasets.forEach((t, a) => {
+    //       let r = e.getDatasetMeta(a)
+    //       r.hidden ||
+    //         r.data.forEach((e, r) => {
+    //           // 昨天数据发灰
+    //           ctx.setFillStyle(r === 0 ? '#e0e0e0' : '#ffffff')
 
+    //           let i = t.data[r].toString() + '\xb0'
+    //           let o = e.tooltipPosition()
+    //           0 == a ? ctx.fillText(i, o.x + 2, o.y - 8 - 10) : 1 == a && ctx.fillText(i, o.x + 2, o.y + 8 + 10)
+    //         })
+    //     })
+    //   }
+    // })
+    return new Chart(ctx, getChartConfig(weeklyData))
+  }
 })
