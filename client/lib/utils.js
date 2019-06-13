026dd15693030eb95b9d913d2f38d8dfdeca1aa0
XiaoCai20180701 <421586311@qq.com>
2019-06-13 16:13:26 +0800

    添加 雨雪效果的方法 drawEffect

--- client/lib/utils.js
+++ client/lib/utils.js
@@ -1,5 +1,16 @@
-export const fixChart = (ctx, width, height) => {
+import effect from '../lib/effect'
 
+//绘制雨雪效果
+export const drawEffect = (canvasId, name, width, height, amount) => {
+  let rain = effect(name, wx.createCanvasContext(canvasId), width, height, {
+    amount: amount || 100,
+    speedFactor: 0.03
+  })
+  return rain.run()
+}
+
+
+export const fixChart = (ctx, width, height) => {
   ctx.canvas = {
     //微信小程序中没有canvas对象，我们造一个
     width,
@@ -14,43 +25,41 @@ export const fixChart = (ctx, width, height) => {
 
   ctx.canvas.getAttribute = function(name) {
     //添加canvas对象的属性
-    if(name == 'width'){
+    if (name == 'width') {
       return ctx.canvas.width
     }
-    if(name == 'height'){
+    if (name == 'height') {
       return ctx.canvas.height
     }
   }
 }
 
-export const getChartConfig = (data)=>{
-  data = getChartData(data) 
+export const getChartConfig = (data) => {
+  data = getChartData(data)
   //配置 chart
   let lineData = {
     labels: data.dates,
-    datasets: [
-      {
-        data: data.maxData,
-        fill: false,
-        borderWidth: 2,
-        borderColor: '#FFB74D',
-        pointStyle: 'circle',
-        pointRadius: 3,
-        pointBackgroundColor: '#FFB74D',
-        pointBorderWidth: 1,
-        lineTension: 0.5
-      },{
-        data: data.minData,
-        fill: false,
-        borderWidth: 2,
-        borderColor: '#4FC3F7',
-        pointStyle: 'circle',
-        pointRadius: 3,
-        pointBackgroundColor: '#4FC3F7',
-        pointBorderWidth: 1,
-        lineTension: 0.5
-      }
-    ]
+    datasets: [{
+      data: data.maxData,
+      fill: false,
+      borderWidth: 2,
+      borderColor: '#FFB74D',
+      pointStyle: 'circle',
+      pointRadius: 3,
+      pointBackgroundColor: '#FFB74D',
+      pointBorderWidth: 1,
+      lineTension: 0.5
+    }, {
+      data: data.minData,
+      fill: false,
+      borderWidth: 2,
+      borderColor: '#4FC3F7',
+      pointStyle: 'circle',
+      pointRadius: 3,
+      pointBackgroundColor: '#4FC3F7',
+      pointBorderWidth: 1,
+      lineTension: 0.5
+    }]
   }
   return {
     type: 'line',
@@ -69,16 +78,12 @@ export const getChartConfig = (data)=>{
         display: false
       },
       scales: {
-        yAxes: [
-          {
-            display: false
-          }
-        ],
-        xAxes: [
-          {
-            display: false
-          }
-        ]
+        yAxes: [{
+          display: false
+        }],
+        xAxes: [{
+          display: false
+        }]
       },
       layout: {
         padding: {
@@ -92,12 +97,16 @@ export const getChartConfig = (data)=>{
   }
 }
 
-export const getChartData = (data)=>{
+export const getChartData = (data) => {
   let dates = [],
-  maxData = [],
-  minData = []
-  if(data && data.length){
-    data.forEach(({date,maxTemp,minTemp})=>{
+    maxData = [],
+    minData = []
+  if (data && data.length) {
+    data.forEach(({
+      date,
+      maxTemp,
+      minTemp
+    }) => {
       dates.push(date)
       maxData.push(maxTemp)
       minData.push(minTemp)
