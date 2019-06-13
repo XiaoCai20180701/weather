84c85d61ea692b273c7afba4ba25ed0658725a37
XiaoCai20180701 <421586311@qq.com>
2019-06-13 16:20:27 +0800

    使用和风天气中应用的密钥

--- server/cloud-functions/he-weather/index.js
+++ server/cloud-functions/he-weather/index.js
@@ -1,8 +1,19 @@
+const cloud = require('wx-server-sdk')
+cloud.init()
+
 const API_URL = 'https://free-api.heweather.net/s6/weather'
-const KEY = '39a99e9fec4d47f8911a7be8235e29d1'
+// const KEY = '39a99e9fec4d47f8911a7be8235e29d1'
 const request = require('request')
 
+// const $ = require('../../inline/utils')
+// 引入云函数功能工具方法，跟空气质量公用
+// gulp prod 打包的时候将公共 utils 库嵌入式引入
+/*<jdists import="../../inline/utils.js" />*/
+
+// 普通 mock server 的代码直接将 utils 库当模块引入
+/*<remove>*/
 const $ = require('../../inline/utils')
+/*</remove>*/
 
 
 exports.main = async (event) => {
@@ -10,7 +21,7 @@ exports.main = async (event) => {
   let location = `${lat},${lon}`
   let params = {
     location,
-    key: KEY //和风天气中应用的密钥
+    key: '39a99e9fec4d47f8911a7be8235e29d1' //和风天气中应用的密钥
   }
   let query = []
   for (let i in params) {
